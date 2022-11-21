ed53edba71c588d1fbe1c60ba8cd5b28ded83056
Esteban Ramirez Hincapie <ceramirez@posteo.net>
2022-11-21 16:54:30 +0100

    Line Charts Working / Data from all 7 countries in place

--- src/Pages/Population.tsx
+++ src/Pages/Population.tsx
@@ -4,63 +4,73 @@ import { Bar } from "react-chartjs-2";
 import TestPage1 from "./TestPage1";
 import LineChart from "../Components/LineChart";
 import NL from "../Data/nederlands.json";
+import CO from "../Data/colombia.json";
+import PO from "../Data/poland.json";
+import CH from "../Data/china.json";
+import VI from "../Data/vietnam.json";
+import US from "../Data/usa.json";
+import JP from "../Data/japan.json";
 
 const Popultaion = () => {
   const nlData = NL.Netherlands.data;
-  const dataPoints = [
-    {
-      year: 2010,
-      population: 16682928,
-    },
-    {
-      year: 2011,
-      population: 16738191,
-    },
-    {
-      year: 2012,
-      population: 16791850,
-    },
-    {
-      year: 2013,
-      population: 16843512,
-    },
-    {
-      year: 2014,
-      population: 16892518,
-    },
-    {
-      year: 2015,
-      population: 16938492,
-    },
-    {
-      year: 2016,
-      population: 16981286,
-    },
-    {
-      year: 2017,
-      population: 17021344,
-    },
-    {
-      year: 2018,
-      population: 17059560,
-    },
-    {
-      year: 2019,
-      population: 17097124,
-    },
-    {
-      year: 2020,
-      population: 17134874,
-    },
-  ];
+  const coData = CO.Colombia.data;
+  const poData = PO.Poland.data;
+  const chData = CH.China.data;
+  const viData = VI.Vietnam.data;
+  const usData = US["United States"].data;
+  const jpData = JP.Japan.data;
+
   const [userData, setUserData] = useState({
     labels: nlData.map((data) => data.year),
     datasets: [
       {
-        label: "population growth",
+        label: "Japan",
+        data: jpData.map((data) => data.population),
+        borderColor: "orange",
+        borderWidth: 0.2,
+      },
+
+      {
+        label: "Vietnam",
+        data: viData.map((data) => data.population),
+        borderColor: "orange",
+        borderWidth: 0.2,
+      },
+      {
+        label: "Netherlands",
         data: nlData.map((data) => data.population),
-        borderColor: "black",
-        borderWidth: 2,
+        borderColor: "orange",
+        borderWidth: 0.2,
+      },
+      {
+        label: "Colombia",
+        data: coData.map((data) => data.population),
+        borderColor: "blue",
+        borderWidth: 0.2,
+      },
+      {
+        label: "Poland",
+        data: poData.map((data) => data.population),
+        borderColor: "red",
+        borderWidth: 0.2,
+      },
+    ],
+  });
+
+  const [usChData, setUsCHData] = useState({
+    labels: usData.map((data) => data.year),
+    datasets: [
+      {
+        label: "USA",
+        data: usData.map((data) => data.population),
+        borderColor: "orange",
+        borderWidth: 0.2,
+      },
+      {
+        label: "China",
+        data: chData.map((data) => data.population),
+        borderColor: "orange",
+        borderWidth: 0.2,
       },
     ],
   });
@@ -68,8 +78,9 @@ const Popultaion = () => {
   return (
     <div>
       <Title>This is Population</Title>
-      <div style={{ width: 700 }}>
+      <div style={{ width: 1000 }}>
         <LineChart chartData={userData} />
+        <LineChart chartData={usChData} />
       </div>
       {/* <TestPage1 /> */}
     </div>
