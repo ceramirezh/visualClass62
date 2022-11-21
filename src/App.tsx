8a6e1720c38fe8d2b8b2dbb6c702b74d0c60c2ff
Esteban Ramirez Hincapie <ceramirez@posteo.net>
2022-11-21 11:50:45 +0100

    Side Navigation Bar implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./Components/Navbar";
 
 function App() {
   return (
     <div>
       <h1>Start Project</h1>
+      <NavBar>
+        <Routes>
+          <Route />
+        </Routes>
+      </NavBar>
     </div>
   );
 }
