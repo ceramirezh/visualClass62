398fe83cb486ae075a42b02402615efac53c9cf2
Esteban Ramirez Hincapie <ceramirez@posteo.net>
2022-11-24 16:33:57 +0100

    Population Chart Cleaned

--- src/Data/IntialData.tsx
+++ src/Data/IntialData.tsx
@@ -9,13 +9,44 @@ import JP from "../Data/japan.json";
 export const initialCountries = [
   {
     name: "Netherlands",
-    backgroundColor: "rgba(255, 99, 132, 0.5)",
     data: NL.Netherlands.data,
+    backgroundColor: "rgba(255, 99, 132, 0.5)",
+    borderColor: "",
   },
-  { name: "Colombia", backgroundColor: "#ff00ff", data: CO.Colombia.data },
-  { name: "Poland", backgroundColor: "#0000ff", data: PO.Poland.data },
-  { name: "China", backgroundColor: "#ff00ff", data: CH.China.data },
-  { name: "Vietnam", backgroundColor: "#33cec6", data: VI.Vietnam.data },
-  { name: "USA", backgroundColor: "#00ff0d", data: US["United States"].data },
-  { name: "Japan", backgroundColor: "#ffe600", data: JP.Japan.data },
+  {
+    name: "Colombia",
+    data: CO.Colombia.data,
+    backgroundColor: "#ff00ff",
+    borderColor: "#000000",
+  },
+  {
+    name: "Poland",
+    data: PO.Poland.data,
+    backgroundColor: "#0000ff",
+    borderColor: "#000000",
+  },
+  {
+    name: "China",
+    data: CH.China.data,
+    backgroundColor: "#ff00ff",
+    borderColor: "#000000",
+  },
+  {
+    name: "Vietnam",
+    data: VI.Vietnam.data,
+    backgroundColor: "#33cec6",
+    borderColor: "#000000",
+  },
+  {
+    name: "USA",
+    data: US["United States"].data,
+    backgroundColor: "#00ff0d",
+    borderColor: "#000000",
+  },
+  {
+    name: "Japan",
+    data: JP.Japan.data,
+    backgroundColor: "#ffe600",
+    borderColor: "#000000",
+  },
 ];
