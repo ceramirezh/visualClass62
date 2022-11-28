e6877e6f620eb5d62813171af623b1bfcf919297
Esteban Ramirez Hincapie <ceramirez@posteo.net>
2022-11-28 13:47:04 +0100

    explotion of colors added using d3

---
+++ src/TryCss.txs/ForMyUnderstanding.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+const ForMyUnderstanding = () => {
+  return (
+    <>
+      <section className="section">
+        <h1>Event Colums</h1>
+      </section>
+    </>
+  );
+};
+
+export default ForMyUnderstanding;
